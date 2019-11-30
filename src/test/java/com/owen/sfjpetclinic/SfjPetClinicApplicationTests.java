54f26c594fad5d10243f97d5e00b5b65de5bd02f
Owen Li <owenli2007@hotmail.com>
2019-11-30 13:50:10 +0800

    init2

---
+++ src/test/java/com/owen/sfjpetclinic/SfjPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owen.sfjpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfjPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
