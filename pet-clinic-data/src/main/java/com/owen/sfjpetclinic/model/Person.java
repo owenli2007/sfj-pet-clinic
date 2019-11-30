3abc0783a923876ce8c2c44f2d3a4b3f71a72936
Owen Li <owenli2007@hotmail.com>
2019-11-30 20:46:43 +0800

    add base entity. closes #10.

--- pet-clinic-data/src/main/java/com/owen/sfjpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/owen/sfjpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.owen.sfjpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 	private String firstName;
 	private String lastName;
 	
--- pet-clinic-data/src/main/java/com/owen/sfjpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/owen/sfjpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.owen.sfjpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 	private String name;
 
 	public String getName() {
