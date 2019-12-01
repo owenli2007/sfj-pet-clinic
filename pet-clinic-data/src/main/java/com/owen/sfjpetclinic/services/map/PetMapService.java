586d00227c91b41ec1dbf0b385234107ba6362c2
Owen Li <owenli2007@hotmail.com>
2019-12-01 16:40:49 +0800

    refactored ID to autogen. closed #23.

--- pet-clinic-data/src/main/java/com/owen/sfjpetclinic/services/map/PetMapService.java
+++ pet-clinic-data/src/main/java/com/owen/sfjpetclinic/services/map/PetMapService.java
@@ -13,7 +13,7 @@ public class PetMapService extends AbstractMapService<Pet, Long> implements PetS
 	@Override
 	public Pet save(Pet object) {
 		// TODO Auto-generated method stub
-		return super.save(object.getId(), object);
+		return super.save(object);
 	}
 	
 	@Override
