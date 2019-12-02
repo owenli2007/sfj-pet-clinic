dfd636c514e138cc7cfac701898fbcb893b8a029
Owen Li <owenli2007@hotmail.com>
2019-12-02 17:15:58 +0800

    added pets and contact info to owners on startup. Closes #33.

--- pet-clinic-data/src/main/java/com/owen/sfjpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/com/owen/sfjpetclinic/services/map/OwnerMapService.java
@@ -5,10 +5,23 @@ import java.util.Set;
 import org.springframework.stereotype.Service;
 
 import com.owen.sfjpetclinic.model.Owner;
+import com.owen.sfjpetclinic.model.Pet;
 import com.owen.sfjpetclinic.services.OwnerService;
+import com.owen.sfjpetclinic.services.PetService;
+import com.owen.sfjpetclinic.services.PetTypeService;
 
 @Service
 public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService {
+
+	private final PetTypeService petTypeService;
+	private final PetService petService;
+	
+	
+	public OwnerMapService(PetTypeService petTypeService, PetService petService) {
+		super();
+		this.petTypeService = petTypeService;
+		this.petService = petService;
+	}
 
 	@Override
 	public Set<Owner> findAll() {
@@ -23,7 +36,30 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
 
 	@Override
 	public Owner save(Owner object) {
-		return super.save(object);
+		
+		if (object != null) {
+			if (object.getPets() != null) {
+				object.getPets().forEach(pet -> {
+					if (pet.getPetType() != null) {
+						if (pet.getPetType().getId() == null) {
+							pet.setPetType(petTypeService.save(pet.getPetType()));
+						}
+					}
+					else {
+						throw new RuntimeException("Pet Type is required");
+					}
+					
+					if (pet.getId() == null) {
+						Pet savedPet = petService.save(pet);
+						pet.setId(savedPet.getId());
+					}
+				});
+			}
+			return super.save(object);
+		}
+		else {
+			return null;
+		}
 	}
 
 	@Override
