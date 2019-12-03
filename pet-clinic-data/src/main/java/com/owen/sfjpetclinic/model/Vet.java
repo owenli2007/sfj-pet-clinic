e1542ea6da4196b5a66379e5ca987ffa17633385
Owen Li <owenli2007@hotmail.com>
2019-12-03 17:30:50 +0800

    updated vet to JPA annotations. closes #37

--- pet-clinic-data/src/main/java/com/owen/sfjpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/owen/sfjpetclinic/model/Vet.java
@@ -3,8 +3,20 @@ package com.owen.sfjpetclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "vet_specialty", joinColumns = @JoinColumn(name = "vet_id"),
+			inverseJoinColumns = @JoinColumn(name = "specialty_id"))
 	private Set<Specialty> specialities = new HashSet<>();;
 
 	public Set<Specialty> getSpecialities() {
