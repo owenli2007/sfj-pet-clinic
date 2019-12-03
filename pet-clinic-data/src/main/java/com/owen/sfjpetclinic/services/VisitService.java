74580314983d0402fbcb20b408a23de8d3a4f5be
Owen Li <owenli2007@hotmail.com>
2019-12-03 19:36:16 +0800

    added VisitMapService. closes #46.

---
+++ pet-clinic-data/src/main/java/com/owen/sfjpetclinic/services/VisitService.java
@@ -0,0 +1,8 @@
+package com.owen.sfjpetclinic.services;
+
+
+import com.owen.sfjpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+	
+}
---
+++ pet-clinic-data/src/main/java/com/owen/sfjpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,37 @@
+package com.owen.sfjpetclinic.services.map;
+
+import java.util.Set;
+
+import com.owen.sfjpetclinic.model.Pet;
+import com.owen.sfjpetclinic.model.Visit;
+import com.owen.sfjpetclinic.services.VisitService;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+	@Override
+	public Visit save(Visit visit) {
+		if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null) {
+			throw new RuntimeException("Invalid visit");
+		}
+		return super.save(visit);
+	}
+	
+	@Override
+	public Set<Visit> findAll() {
+		return super.findAll();
+	}
+	
+	@Override
+	public Visit findById(Long id) {
+		return super.findById(id);
+	}
+	
+	@Override
+	public void delete(Visit object) {
+		super.delete(object);
+	}
+	
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+}
